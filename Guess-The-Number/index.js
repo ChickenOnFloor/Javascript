d3a7e7f7469dc34c90d503e361a1874d314d05f0
Jawwad <jawwadsiddique0987@gmail.com>
2024-12-18 00:37:28 +0500

    Made couple changes in Index.js

--- Guess-The-Number/index.js
+++ Guess-The-Number/index.js
@@ -2,6 +2,7 @@ var limit = 10
 var number = 0
 function randomGenerate(){
     let num = Math.random() * limit + 1
+    console.log(Math.floor(num))
     return Math.floor(num)
 }
 number = randomGenerate()
@@ -14,7 +15,8 @@ var submitted = false
 function submit(){
     if(!submitted){
         const element = document.querySelector('input')
-        if(parseInt(element.value) === number){
+        let ans = parseInt(element.value)
+        if(ans === number){
             submitted = true
             element.type = "text"
             element.value = "Correct"
@@ -27,13 +29,28 @@ function submit(){
         }
         else{
             submitted = true
-            element.type = "text"
-            element.value = "Incorrect"
-            setTimeout(() => {
-                element.type = "number"
-                element.value = null
-                submitted = false
-            }, 2000);
+            if(ans && ans < number){
+                element.type = "text"
+                element.value = "Go Higher"
+                element.style.fontSize = "4vw"
+                setTimeout(() => {
+                    element.type = "number"
+                    element.value = null
+                    submitted = false
+                    element.style.fontSize = "5vw"
+                }, 2000);
+            }
+            else if(ans && ans > number){
+                element.type = "text"
+                element.value = "Go Lower"
+                element.style.fontSize = "4vw"
+                setTimeout(() => {
+                    element.type = "number"
+                    element.value = null
+                    submitted = false
+                    element.style.fontSize = "5vw"
+                }, 2000);
+            }
         }
     }
 }
