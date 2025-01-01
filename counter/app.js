03df9d1648d9135bc7ccce50d99e5867a1c46614
Muhammad Jawwad Siddique <115373612+ChickenOnFloor@users.noreply.github.com>
2025-01-01 14:23:03 +0500

    Update app.js

--- counter/app.js
+++ counter/app.js
@@ -3,6 +3,9 @@ const remove = document.querySelector('.sub');
 const dark = document.querySelectorAll('.dark-tog');
 const darktoggle = document.querySelector('.darktoggle');
 const num = document.querySelector('.count');
+const changeColor = () => {
+    num.style.color = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+}
 darktoggle.addEventListener('click', () => {
     if(darktoggle.innerHTML === 'Light Mode<i class="fa-solid fa-moon"></i>'){
         darktoggle.innerHTML = 'Dark Mode<i class="fa-solid fa-sun"></i>';
@@ -17,9 +20,11 @@ darktoggle.addEventListener('click', () => {
 
 add.addEventListener('click', () => {
     num.innerHTML = parseInt(num.innerHTML) + 1;
+    changeColor()
 });
 
 remove.addEventListener('click', () => {
     let number = Number(num.innerHTML);
+    changeColor()
     num > 0 ? num.innerHTML = parseInt(num.innerHTML) - 1 : num.innerHTML = 0;
 });
